b5b5986dd2f7493fa8e619c9fe44437b14a54450
Lars Emil <larseknu@hiof.no>
2023-01-10 12:49:33 +0100

    Eksempler til Forelesning 01

---
+++ Forelesning01_Uke02_Introduksjon/HeiVerden.java
@@ -0,0 +1,14 @@
+/**
+* Den første klassen man lager i Java
+* HeiVerden er navnet på klassen, og java-filen må derfor også hete det samme.
+*/
+public class HeiVerden {
+  /**
+  * Javaapplikasjonens startpunkt (denne metoden blir kjørt når java-applikasjonen kjøres)
+  */
+  public static void main(String[] args) {
+    // Printer ut teksten "Hei, verden!" til terminalen
+    System.out.println("Hei, verden!");
+  }
+
+}
